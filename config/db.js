00f4d6d612f5dac0b6156de7e6b2498290934787
priya <priyaannamalaisamy@gmail.com>
2026-07-17 18:24:16 +0530

    Connect backend to TiDB Cloud

--- config/db.js
+++ config/db.js
@@ -1,10 +1,14 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Arunpriya@#2004",
-  database: "arunpriya_mehendi"
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: true,
+  },
 });
 
 db.connect((err) => {
